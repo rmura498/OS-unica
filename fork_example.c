672b43c828757c0082d603a1c2ec12a04a9d1c22
rmura498 <76777353+rmura498@users.noreply.github.com>
2022-01-28 17:14:09 +0100

    forking examples

--- fork_example.c
+++ fork_example.c
@@ -4,24 +4,36 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
 
 int main(int argc, char **argv)
 {
     printf("The parent PID is: %d\n", getpid());
+    int pid;
     int a,b,c;
     a=3;
     b=-2;
     c=5;
 
-    if(fork()==0)
+    if( (pid= fork()) == -1)
     {
-        printf("The child PID is: %d\n", getpid());
-        printf("CHILD: a = %d\n b = %d\n c = %d\n", a-2, b+1, c-7);
+        perror("Error in the fork() call");
+        exit(-1);
     }
-    else
-    {
 
-        printf("PARENT: a = %d\n b = %d\n c = %d\n", a, b, c);
+    switch (pid) {
+        case 0:
+            printf("The child PID is: %d\n", getpid());
+            printf("CHILD: a = %d\n b = %d\n c = %d\n", a-2, b+1, c-7);
+            break;
+
+        default:
+            printf("PARENT: a = %d\n b = %d\n c = %d\n", a, b, c);
+            break;
+
+
     }
-    printf("[%d] Bye\n\n", getpid());
+
+
 }
