9c3fa1408b66e831648b1aff7cdab8e7da90422b
rmura498 <76777353+rmura498@users.noreply.github.com>
2022-02-22 18:44:55 +0100

    named_pipes2

---
+++ Lab6/part3-1dic/SingleFileProcessingServerSTOP.c
@@ -0,0 +1,104 @@
+//
+// Created by Raffaele Mura on 21/02/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "server.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+
+
+typedef struct {
+    char subject[30];
+    int mark;
+    int credits;
+} transcript;
+
+
+int main(int argc, char **argv) {
+
+    FILE *fp;
+    int N;
+    int i;
+    transcript t[50];
+    int SumWeightedMarks, SumCredits, MaxMark;
+    float avg;
+
+    int fd_pipe;
+    int client_pipe;
+    message m;
+    int bytes_read;
+
+    char output[PIPE_BUF];
+
+    if (mkfifo(PIPE, 0666) == -1) {
+        perror("Cannot create the named pipe");
+        exit(-1);
+    }
+    if ((fd_pipe = open(PIPE, O_RDWR)) == -1) {
+        perror("Cannot open the named pipe");
+        exit(-1);
+    }
+
+    while((bytes_read=read(fd_pipe, &m, sizeof(message)))!=0)
+    {
+        if(strcmp(m.filename, "STOP")==0)
+        {
+            close(fd_pipe);
+            unlink(PIPE);
+            if((client_pipe=open(m.private_pipe,O_WRONLY))==-1)
+            {
+                perror("Unable to open the private pipe");
+                exit(-1);
+            }
+
+            sprintf(output, "The server is shutting down");
+            write(client_pipe, output, strlen(output));
+            close(client_pipe);
+            exit(-1);
+        }
+        if ((fp = fopen(m.filename, "r")) == NULL) {
+            perror("Unable to open the file");
+            exit(-1);
+        }
+
+        fscanf(fp, "%d", &N);
+
+        for (i = 0; i < N; i++)
+            fscanf(fp, "%s %d %d", t[i].subject, &t[i].mark, &t[i].credits);
+
+        SumWeightedMarks = 0;
+        SumCredits = 0;
+        MaxMark = 0;
+
+
+        for (i = 0; i < N; i++) {
+            SumWeightedMarks += t[i].mark * t[i].credits;
+            SumCredits += t[i].credits;
+            if (MaxMark < t[i].mark)
+                MaxMark = t[i].mark;
+
+        }
+
+        avg = (float) SumWeightedMarks / SumCredits;
+
+        if((client_pipe=open(m.private_pipe,O_WRONLY))==-1)
+        {
+            perror("Unable to open the private pipe");
+            exit(-1);
+        }
+
+        sprintf(output, "Student %s: Average: %f and Maximum Mark: %d", m.filename, avg, MaxMark);
+        write(client_pipe, output, strlen(output));
+        close(client_pipe);
+    }
+
+}
+
+
+
+
